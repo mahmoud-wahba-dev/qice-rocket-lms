6257f3eccc0bdfa4aeb80d550225c451d54bae4a
mahmoud.wahba.dev <mahmoud.wahba.dev@gmail.com>
2026-08-26 16:34:33 +0300

    update

--- resources/views/panel_v1/student/pages/home.blade.php
+++ resources/views/panel_v1/student/pages/home.blade.php
@@ -1,8 +1,32 @@
 @extends('panel_v1.student.layouts.app')
 
 @section('content')
-    <div class="panel-v1-placeholder">
-        <h1>لوحة المتدرب</h1>
-        <p>هيكل لوحة المتدرب (panel_v1) — سيتم بناء محتوى لوحة التحكم وفق تصميم Figma لاحقاً.</p>
+<header class="bg-primary text-white pt-8 max-xl:mt-20 h-[70vh]">
+    <div class="container">
+        <div class="px-15 py-16">
+            <h1 class="font-bold text-40px  mb-1">طاب مساؤك ، علا محمد</h1>
+            <p class="font-semibold text-20px mb-15">جاهزة لاستكمال دورة "قياس النجاح والجودة" اليوم؟</p>
+
+            <div class="grid grid-cols-1 xl:grid-cols-5">
+                <div class="bg-white/16 rounded-12px">
+                    <div class="mb-3 flex items-center gap-2 gap-2">
+                        <span class="font-semibold text-40px">4</span>
+                        <svg width="32" height="27" viewBox="0 0 32 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M5.33333 12.444L0 9.33333L16 0L32 9.33333V20.6667H29.3333V10.8893L26.6667 12.444V21.348L26.3693 21.7147C25.1208 23.2624 23.5412 24.5107 21.7468 25.3677C19.9523 26.2246 17.9886 26.6685 16 26.6667C14.0114 26.6685 12.0477 26.2246 10.2532 25.3677C8.45878 24.5107 6.87921 23.2624 5.63067 21.7147L5.33333 21.348V12.444ZM8 14V20.3893C8.99994 21.5256 10.2308 22.4354 11.6103 23.058C12.9899 23.6806 14.4864 24.0018 16 24C17.5136 24.0018 19.0101 23.6806 20.3897 23.058C21.7692 22.4354 23.0001 21.5256 24 20.3893V14L16 18.6667L8 14ZM5.29333 9.33333L16 15.58L26.7067 9.33333L16 3.08667L5.29333 9.33333Z"
+                                fill="#C99C69" />
+                        </svg>
+
+                    </div>
+
+                    <p class="font-semibold text-18px">الدورات النشطة</p>
+
+                </div>
+            </div>
+        </div>
     </div>
+    <div>
+        <img src="" alt="">
+    </div>
+</header>
 @endsection
